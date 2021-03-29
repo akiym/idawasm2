5bdad4aa019e9ca50485803f59759fb9d233f58d
Takumi Akiyama <t.akiym@gmail.com>
2021-03-30 07:19:37 +0900

    use collections.abc.Iterator

--- stubs/wasm/decode.pyi
+++ stubs/wasm/decode.pyi
@@ -2,7 +2,8 @@ from .compat import byte2int as byte2int
 from .modtypes import ModuleHeader as ModuleHeader, NameSubSection as NameSubSection, SEC_NAME as SEC_NAME, SEC_UNK as SEC_UNK, Section as Section
 from .opcodes import OPCODE_MAP as OPCODE_MAP
 from collections import namedtuple
-from typing import Any, Iterator
+from collections.abc import Iterator
+from typing import Any
 
 Instruction = namedtuple('Instruction', 'op imm len')
 
